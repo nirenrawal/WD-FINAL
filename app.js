c7a31bbe4d30142dc5c7fb6b9e489c2dc3cc7c38
nirenrawal <nire0007@stud.kea.dk>
2022-05-03 09:19:32 +0200

    app.js, signup html changed

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 
-window.addEventListener(
-    'DOMContentLoaded', function () {
-        const overlay = document.querySelector('#overlay')
-        const signupBtn = document.querySelector('#signup-btn')
-        const closeBtn = document.querySelector('#close-btn')
-        const overlayLogin = document.querySelector("#overlay-login")
-        const loginBtn = document.querySelector("#login-btn")
-        const closeBtnLogin = document.querySelector("#close-btn-login")
+// window.addEventListener(
+//     'DOMContentLoaded', function () {
+//         const overlay = document.querySelector('#overlay')
+//         const signupBtn = document.querySelector('#signup-btn')
+//         const closeBtn = document.querySelector('#close-btn')
+//         const overlayLogin = document.querySelector("#overlay-login")
+//         const loginBtn = document.querySelector("#login-btn")
+//         const closeBtnLogin = document.querySelector("#close-btn-login")
     
 
-        // signupBtn.addEventListener('click', function() {
-        //     overlay.classList.remove('hidden')
-        //     overlay.classList.add('flex')
-        // })
+//         // signupBtn.addEventListener('click', function() {
+//         //     overlay.classList.remove('hidden')
+//         //     overlay.classList.add('flex')
+//         // })
 
-        closeBtn.addEventListener('click', function() {
-            overlay.classList.remove('flex')
-            overlay.classList.add('hidden')
-        })
+//         closeBtn.addEventListener('click', function() {
+//             overlay.classList.remove('flex')
+//             overlay.classList.add('hidden')
+//         })
 
-        // loginBtn.addEventListener('click', function() {
-        //     overlayLogin.classList.remove('hidden')
-        //     overlayLogin.classList.add('flex')
-        // })
+//         // loginBtn.addEventListener('click', function() {
+//         //     overlayLogin.classList.remove('hidden')
+//         //     overlayLogin.classList.add('flex')
+//         // })
 
-        // closeBtnLogin.addEventListener('click', function() {
-        //     overlayLogin.classList.remove('flex')
-        //     overlayLogin.classList.add('hidden')
-        // })
+//         // closeBtnLogin.addEventListener('click', function() {
+//         //     overlayLogin.classList.remove('flex')
+//         //     overlayLogin.classList.add('hidden')
+//         // })
 
-    }
-)
+//     }
+// )
+
+
+document.getElementById('user_image').value= null;
